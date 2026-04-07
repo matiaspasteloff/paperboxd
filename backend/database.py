324235fc2506465ec9fe42f168f2119e2cc09256
paste-fiuba <mpasteloff@fi.uba.ar>
2026-04-06 22:37:24 -0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,15 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10,
+    connect_args={"sslmode": "require"}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
